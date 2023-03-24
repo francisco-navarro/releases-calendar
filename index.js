53dbac6eeff622e3f94d6e31b41e3c7a2d6033ae
Paco <franciscojose.navarro@bbva.com>
2023-03-24 08:28:26 +0100

    heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("src"));
+app.use(express.static("dist"));
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
